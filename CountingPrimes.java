b79e88ca809946e0586136386cce2be12ee2aa3e
Mark Conley <m.conleyy@gmail.com>
2014-06-16 00:45:46 -0400

    Update CountingPrimes.java

--- CountingPrimes.java
+++ CountingPrimes.java
@@ -51,7 +51,7 @@ public class CountingPrimes {
 
 	static boolean isPrime(int num) {
 		if (num <= 1) {	return false; }
-		for (int i = 2; i <= num / 2; i++) {
+		for (int i = 2; i <= Math.sqrt(num); i++) {
 			if (num % i == 0) {
 				return false;
 			}
