1cc894988fec46d022fafd01e15da1cf1c48c469
Mark Conley <m.conleyy@gmail.com>
2014-06-20 19:07:41 -0400

    commented out unnecessary checks.
    squeezing out efficiency

--- PrimePalinedrone.java
+++ PrimePalinedrone.java
@@ -9,7 +9,7 @@ package codeeval;
 public class PrimePalindrone {
 	static int largestPP;
 	public static void main(String[] args) {
-		for (int i = 2; i < 1000; i++) {
+		for (int i = 10; i < 1000; i++) {
 			if (isPrime(i)) {
 				isPalindrone(i);
 			}
@@ -18,21 +18,19 @@ public class PrimePalindrone {
 	}
 
 	static boolean isPrime(int num) {
-		if (num <= 1) { return false; }
+		/*
+		if (num <= 1) return false; 
+		*/
 		for (int i = 2; i <= Math.sqrt(num); i++) {
-			if (num % i == 0) {
-				return false;
-			}
+			if (num % i == 0) return false; 
 		}
 		return true;
 	}
 
 	static void isPalindrone(int prime) {
 		String test = Integer.toString(prime);
-		
-		if (test.length() == 1) {
-			return;
-		} else if (test.length() == 2) {
+
+		if (test.length() == 2) {
 			if (test.charAt(0) == test.charAt(1)) {
 				largestPP = prime;
 			}
