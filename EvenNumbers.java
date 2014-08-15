92113b08ba30d69503fcf3f32fa1e012b5af616e
Mark Conley <m.conleyy@gmail.com>
2014-08-15 00:00:38 -0400

    Create EvenNumbers.java

---
+++ EvenNumbers.java
@@ -0,0 +1,32 @@
+package codeeval;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class EvenNumbers {
+	public static void main(String[] args) {
+		
+		File file;
+		BufferedReader br;
+		String line;
+		int c;
+		
+		try {
+			file = new File(args[0]);
+			br = new BufferedReader(new FileReader(file));
+			while ((line = br.readLine()) != null ){
+				c = Integer.parseInt(line);
+				if(c%2 == 0){
+					System.out.println(1);
+				} else {
+					System.out.println(0);
+				}
+			}
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+	}
+}
