933a1ca1ed58cf292b441be8a461f10aa1724ab3
Mark Conley <m.conleyy@gmail.com>
2015-06-16 19:23:48 -0400

    Update StringMask.java

--- StringMask.java
+++ StringMask.java
@@ -1,38 +1,34 @@
+package codeeval;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
-public class Main {
+public class StringMask {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		File file;
 		BufferedReader reader;
 
-		try {
+		file = new File(args[0]);
+		reader = new BufferedReader(new FileReader(file));
 
-			file = new File(args[0]);
-			reader = new BufferedReader(new FileReader(file));
+		String line = null;
 
-			String line = null;
+		while ((line = reader.readLine()) != null) {
+			StringBuilder result = new StringBuilder(line);
 
-			while ((line = reader.readLine()) != null) {
-				StringBuilder result = new StringBuilder(line);
-				
-				String[] tokens = line.split(" ");
-				String string = tokens[0];
-				String mask = tokens[1];
-				
-				for (int i = 0; i < string.length(); i++) {
-					char ch = string.charAt(i);
-					result.setCharAt(i, mask.charAt(i) == 1 ? Character.toUpperCase(ch) : ch);
-					
-				//	result.setCharAt(i, ch >= 'A' && ch <= 'Z' ? (char) (ch + 'a' - 'A') : ch);
-				}
-				System.out.println(result);
+			String[] tokens = line.split(" ");
+			String string = tokens[0];
+			String mask = tokens[1];
+
+			for (int i = 0; i < string.length(); i++) {
+				result.setCharAt(i, mask.charAt(i) == '1' ? Character.toUpperCase(string.charAt(i)) : string.charAt(i));
 			}
-			reader.close();
-		} catch (Exception e) {
+			System.out.println(result);
 		}
+		reader.close();
+
 	}
 }
