517dcf08a5f36a078d3111b1a3ed59e13a54e994
mc4 <mark.conley@rutgers.edu>
2015-10-18 17:58:20 -0400

    64

---
+++ moderate/card_validation.py
@@ -0,0 +1,24 @@
+import sys
+
+def isOdd(n):
+	return n%2 != 0
+
+def Luhn_formula(card_num):
+
+	for index, num in enumerate(card_num[::-1]):
+		if isOdd(index):
+			tmp = int(num) * 2
+			if tmp > 9:
+				s = int(str(tmp)[0]) + int(str(tmp)[1])
+				card_num[index] = str(s)
+			else:
+				card_num[index] = str(tmp)
+
+	return sum(map(int, card_num)) % 10 == 0
+
+def validate(card_num):
+	return int(Luhn_formula(list(card_num)))
+
+with open(sys.argv[1], 'r') as test_cases:
+	for test in test_cases:
+		print validate(test.strip().replace(' ', ''))
