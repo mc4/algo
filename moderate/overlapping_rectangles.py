b8523dd41511767794553c71c1da2cecb61f408a
mc4 <mark.conley@rutgers.edu>
2015-10-19 18:12:44 -0400

    Is it over??

--- moderate/overlapping_rectangles.py
+++ moderate/overlapping_rectangles.py
@@ -1,16 +1,26 @@
 import sys
 
-with open(sys.argv[1], 'r') as tc:
-	for t in tc:
-		#c = t.strip().split(',')
-		c = [float(_) for _ in t.strip().split(',')]
+def overlap(c):
+	'''Please forgive me for these variable names'''
+	upper_left_x_A  = c[0]  
+	upper_left_y_A  = c[1]   
+	lower_right_x_A = c[2]  
+	lower_right_y_A = c[3]  
 
-		upper_left_x = c[0]
-		upper_left_y = c[1]
-		lower_right_x = c[2]
-		lower_right_y = c[3]
+	upper_left_x_B  = c[4] 
+	upper_left_y_B  = c[5]  
+	lower_right_x_B = c[6]
+	lower_right_y_B = c[7] 
 
-		upper_left_x_B = c[4]
-		upper_left_y_B = c[5]
-		lower_right_x_B = c[6]
-		lower_right_y_B = c[7]
+	if upper_left_x_A > lower_right_x_B or upper_left_x_B > lower_right_x_A:
+		return False
+
+	if upper_left_y_A < lower_right_y_B or upper_left_y_B < lower_right_y_A:
+		return False
+
+	return True
+
+with open(sys.argv[1], 'r') as test_cases:
+	for test in test_cases:
+		c = [float(_) for _ in test.strip().split(',')]
+		print overlap(map(float, test.strip().split(',')))
