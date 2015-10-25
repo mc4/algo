d307d0aefce6349ed3d92cdb8451eb9fec69529f
mc4 <mark.conley@rutgers.edu>
2015-10-24 23:58:08 -0400

    refactor + page2

---
+++ euler/page2/problem56.py
@@ -0,0 +1,16 @@
+def sum_digits(n):
+	s = 0
+	while n:
+		s += n % 10
+		n /= 10
+	return s
+
+maximum = 0
+
+for a in xrange(100):
+	for b in xrange(100):
+		tmp = sum_digits(a ** b)
+		if tmp > maximum:
+			maximum = tmp
+
+print maximum
