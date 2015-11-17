a7b0bfa597478d3c62e0c4b85639f3341b60c576
mc4 <mark.conley@rutgers.edu>
2015-11-17 11:32:21 -0500

    one liners are fun

--- codeeval/easy/one_zeros_two_zeros.py
+++ codeeval/easy/one_zeros_two_zeros.py
@@ -1,15 +1,6 @@
 import sys
 
 def count_zeros(target, limit):
-	'''
-	matches = 0
-	for x in range(1, limit+1):
-		if bin(x)[2:].count('0') == target:
-			matches += 1
-
-	return matches
-	'''
-	
 	return len([x for x in range(1, limit+1) if bin(x)[2:].count('0') == target])
 
 with open(sys.argv[1], 'r') as test_cases:
