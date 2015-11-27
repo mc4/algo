074a79ebd021d5a9b883565384a8cfd1bb5f3403
mc4 <mark.conley@rutgers.edu>
2015-11-27 08:46:36 -0500

    Your runtime beats 2.58% of bash submissions.

--- leetcode/algorithms/hard/valid_number.py
+++ leetcode/algorithms/hard/valid_number.py
@@ -4,9 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        
+
         try:
             float(s)          
             return True
         except:
             return False
+
+# 1481/1481 test cases passed
+# Runtime: 84 ms
