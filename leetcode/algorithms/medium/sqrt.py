e5667f2ea38c55744e1b25c14f9badfb18780034
mc4 <mark.conley@rutgers.edu>
2015-12-06 13:20:10 -0500

    Your runtime beats 81.82% of python submissions.

---
+++ leetcode/algorithms/medium/sqrt.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """      
+        return int(sqrt(x))
+
+# 1017 / 1017 test cases passed.
+# Status: Accepted
+# Runtime: 60 ms
