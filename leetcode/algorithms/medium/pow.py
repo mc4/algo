f270657a0e6c1a80e3aca66bf2d06806b6401c2d
mc4 <mark.conley@rutgers.edu>
2015-12-06 13:22:44 -0500

    Your runtime beats 31.81% of python submissions.

---
+++ leetcode/algorithms/medium/pow.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return x**n
+
+# 299 / 299 test cases passed.
+# Status: Accepted
+# Runtime: 48 ms
