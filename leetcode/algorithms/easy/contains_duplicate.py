810a9e93afc1681050c4810fc383551cb5b836a2
mc4 <mark.conley@rutgers.edu>
2015-12-05 19:51:48 -0500

    Your runtime beats 10.09% of python submissions.

---
+++ leetcode/algorithms/easy/contains_duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if not nums:
+            return False
+        return False if sorted(set(nums)) == sorted(nums) else True
