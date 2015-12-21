f3592691f74f3c779916d22a5381d0fcccd7ee93
mc4 <mark.conley@rutgers.edu>
2015-12-20 23:22:09 -0500

    Your runtime beats 28.57% of python submissions.

--- leetcode/algorithms/medium/linked_list_cycle.py
+++ leetcode/algorithms/medium/linked_list_cycle.py
@@ -22,3 +22,38 @@ class Solution(object):
             if slow == fast: # if two ever meet, must have loop
                 return True
                 
+
+
+# A few other ways to do this:
+#     1. Assuming you can manipulate values of the list and that each 
+#         node has valid value other than None, 
+            
+#             while head
+#                 if head.val == None
+#                     return True
+#                 head.val = None
+#                 head = head.next
+#             return False
+
+
+#         This seems faster, but theres alot of assumptions. 
+#         Kind of cool because it only uses one pointer
+#     2. This might be slightly faster
+
+#             slow = fast = head
+#             while fast and fast.next:
+#                 fast = fast.next.next
+#                 slow = slow.next
+#                 if slow == fast:
+#                     return True
+#             return False
+
+#     3. Fastest?
+#             if head is None: return False
+#             slow, fast = head, head.next
+#             while slow != fast
+#                 if slow is None or fast.next is None: return False
+#                 slow = slow.next
+#                 fast = fast.next.nest
+#             return True
+
