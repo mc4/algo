36396d4c167d84b3858c6c524c50b1655f0f9443
mc4 <mark.conley@rutgers.edu>
2015-12-26 18:53:51 -0500

    Your runtime beats 85.59% of python submissions.

---
+++ leetcode/algorithms/medium/linked_list_cycle_2.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def detectCycle(self, head):
+        
+        finder = slow = fast = head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+            if slow == fast:
+                while finder != slow:
+                    slow = slow.next
+                    finder = finder.next
+                return finder
+                
+        return None
