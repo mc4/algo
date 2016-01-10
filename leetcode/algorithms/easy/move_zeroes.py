1d1ba961cca6323bf534d4c8f5a2d78a10730ef5
mc4 <mark.conley@rutgers.edu>
2016-01-09 22:18:39 -0500

    Your runtime beats 55.48% of python submissions.

--- leetcode/algorithms/easy/move_zeroes.py
+++ leetcode/algorithms/easy/move_zeroes.py
@@ -1,12 +1,27 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        for i, n in enumerate(nums[1:], start = 1):
-            if n == 0:
-                continue
+
+    	# Algorithm 1 runs much faster
+
+    	j, l = 0, len(nums)
+        
+        for i in xrange(l):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+        
+        for i in xrange(j, l):
+            nums[i] = 0
+
+        # # Algorithm 2 r
+
+        # for i, n in enumerate(nums[1:], start = 1):
+        #     if n == 0:
+        #         continue
             
-            j = i
-            while j > 0 and nums[j-1] == 0:
-                j -= 1
+        #     j = i
+        #     while j > 0 and nums[j-1] == 0:
+        #         j -= 1
                 
-            nums[i], nums[j] = 0, n    
+        #     nums[i], nums[j] = 0, n    
         
