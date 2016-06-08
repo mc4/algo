190d5c9fa7298b72985c83e4c6b2090d226874e1
mc4 <mark.conley@rutgers.edu>
2016-06-08 14:56:59 -0400

    Your runtime beats 38.23% of javasubmissions.

---
+++ leetcode/algorithms/hard/LRUCache.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+/**
+ * 
+ * @author MarkC
+ *
+ * Design and implement a data structure for Least Recently Used (LRU) cache. 
+ * It should support the following operations: get and set.
+ * 
+ * get(key) - Get the value (will always be positive) of the key if the key 
+ * 	exists in the cache, otherwise return -1.
+ * 
+ * set(key, value) - Set or insert the value if the key is not already present. 
+ * 	When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+ *
+ */
+
+public class LRUCache extends LinkedHashMap<Integer, Integer> {
+    
+	private int capacity;
+	
+    public LRUCache(int capacity) {
+    	super(capacity, 1.0f, true);
+    	this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+    	if(this.containsKey(key)){
+    		return super.get(key);
+    	}
+        return -1;
+    }
+    
+    public void set(int key, int value) {
+    	if(!(this.containsKey(key)) && size() == capacity){
+    		Set<Integer> keys = this.keySet();
+    		int element = (int) keys.iterator().next();
+    		this.remove(element);
+    	}
+    	super.put(key, value);
+    }
+    
+}
